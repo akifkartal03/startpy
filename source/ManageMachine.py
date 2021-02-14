c12d943dfc414148ea94b1cfffc999a03e8e104a
Akif Kartal <akokartal@gmail.com>
2021-02-14 23:42:58 +0300

    finish project

--- source/ManageMachine.py
+++ source/ManageMachine.py
@@ -8,9 +8,9 @@ from Automat import Automat
 machine = Automat()
 
 
-class ContainersTestCase(unittest.TestCase):
+class AutomatMachineTestCase(unittest.TestCase):
 
-    def test_get_input_stacks_processed_input_correctly(self):
+    def test_machine_runs_correctly(self):
         user_input = ['5', '1', '1', '3', '3', '3', '3', '4', '1', '2', '7',
                       '1', '2', '3', '3', '3', '2', '4', '2', '4', '7',
                       '1', '3', '3', '3', '3', '1', '4', '5', '1', '7',
